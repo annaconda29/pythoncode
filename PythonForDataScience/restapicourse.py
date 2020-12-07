87e47d77f131d9887c3de5298cb86d4e30c95c14
Annapoorni <annapoorni@Sanjeevs-MacBook-Air.local>
2020-12-07 19:54:29 +0900

    Commit

--- PythonForDataScience/restapicourse.py
+++ PythonForDataScience/restapicourse.py
@@ -1,4 +1,11 @@
 import requests
 import json
 response =  requests.get("http://api.stackexchange.com/2.2/questions?order=desc&sort=activity&site=stackoverflow")
-print(response.json())
+for data in response.json()['items']:
+    if data['answer_count'] == 0:
+        print(data['title'])
+        print(data['link'])
+        print()
+    else:
+        print("skipped")
+    print()
