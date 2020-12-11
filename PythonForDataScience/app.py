b2f1781a85899b7656fa7d1103984465ec9559b2
Annapoorni <annapoorni@Sanjeevs-MacBook-Air.local>
2020-12-11 11:47:29 +0900

    Making a Virtual ENV

---
+++ PythonForDataScience/app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "Hello"
+
+@app.route("/drinks")
+def get_drinks():
+    return {"drinks" : "drink_data"}
--- PythonForDataScience/drinks.py
+++ PythonForDataScience/drinks.py
@@ -1,7 +1,17 @@
 from flask import Flask
 app = Flask(__name__)
+from flask_sqlalchemy import SQLAlchemy
 
 
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.db"
+db = SQLAlchemy(app)
+class Drink(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    description = db.column(db.String(120))
+
+    def __repr__(self):
+        return f"{self.name} - {self.description}"
 
 @app.route('/')
 def index():
